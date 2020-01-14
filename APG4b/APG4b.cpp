b0c1dd96f425fddb93d7d2816194adf819ecf482
KtoZ <32612782+KtoZ@users.noreply.github.com>
2020-01-14 23:52:04 +0900

    Add main.cpp file and ignore setting

--- APG4b/APG4b.cpp
+++ APG4b/APG4b.cpp
@@ -1,34 +1,83 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int f0(int N) {
+    return 1;
+}
+
+int f1(int N, int M) {
+    int s = 0;
+    for (int i = 0; i < N; i++) {
+        s++;
+    }
+    for (int i = 0; i < M; i++) {
+        s++;
+    }
+    return s;
+}
+
+int f2(int N) {
+    int s = 0;
+    for (int i = 0; i < N; i++) {
+        int t = N;
+        int cnt = 0;
+        while (t > 0) {
+            cnt++;
+            t /= 2;
+        }
+        s += cnt;
+    }
+    return s;
+}
+
+int f3(int N) {
+    int s = 0;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            s++;
+        }
+    }
+    return s;
+}
+
+int f4(int N) {
+    int s = 0;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            s += i + j;
+        }
+    }
+    return s;
+}
+
+int f5(int N, int M) {
+    int s = 0;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            s += i + j;
+        }
+    }
+    return s;
+}
+
 int main() {
     int N, M;
     cin >> N >> M;
-    vector<int> A(M), B(M);
-    for (int i = 0; i < M; i++) {
-        cin >> A.at(i) >> B.at(i);
-    }
 
-    // ここにプログラムを追記
-    // (ここで"試合結果の表"の2次元配列を宣言)
-    vector<vector<char>> table(N, vector<char>(N, '-'));
-    for (int i = 0; i < M; i++) {
-        int win = A[i] - 1;
-        int lose = B[i] - 1;
-        table[win][lose] = 'o';
-        table[lose][win] = 'x';
-    }
+    int a0 = -1, a1 = -1, a2 = -1, a3 = -1, a4 = -1, a5 = -1;
 
-    for (auto row : table)
-    {
-        for (int i = 0; i < row.size(); i++) {
-            cout << row.at(i);
-            if (i == row.size() - 1) {
-                cout << endl; // 末尾なら改行
-            }
-            else {
-                cout << " ";  // それ以外なら空白
-            }
-        }
-    }
+    // 計算量が最も大きいもの1つだけコメントアウトする
+    a0 = f0(N);
+    a1 = f1(N, M);
+    a2 = f2(N);
+    a3 = f3(N);
+    //a4 = f4(N);
+    a5 = f5(N, M);
+
+    cout << "f0: " << a0 << endl;
+    cout << "f1: " << a1 << endl;
+    cout << "f2: " << a2 << endl;
+    cout << "f3: " << a3 << endl;
+    cout << "f4: " << a4 << endl;
+    cout << "f5: " << a5 << endl;
 }
